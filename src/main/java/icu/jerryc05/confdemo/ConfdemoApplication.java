d14286eb0393d6d4da8505c21e665c093fc3c341
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2023-09-17 13:51:53 -0400

    init commit

---
+++ src/main/java/icu/jerryc05/confdemo/ConfdemoApplication.java
@@ -0,0 +1,13 @@
+package icu.jerryc05.confdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/icu/jerryc05/confdemo/ConfdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.jerryc05.confdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
